8da8589006e97f8185891f6c70a872820cb8202a
Vladislav Maryutin <louietrueman@gmail.com>
2022-06-20 13:21:07 +0300

    14th Sprint 1st attempt

--- app.js
+++ app.js
@@ -41,9 +41,10 @@ app.use((err, req, res, next) => {
     .status(statusCode)
     .send({
       message: statusCode === 500
-        ? 'На сервере произошла ошибка'
+        ? 'На сервере произошла ошибка!!!!!!'
         : message,
     });
 });
+app.use('/', (req, res) => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 
 app.listen(PORT);
